bb5b63f98f661a4010baa57480ccfdea02ea3340
NidhiPatel <nidhi.patel@alphacertlabs.com>
2020-08-02 10:00:46 +1200

    Added nodemon and Find Buddy page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
+app.get("/find", (req, res) => res.sendFile(__dirname + "/public/find.html"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
