c1e1919194c17e5672fd7b47915db4aca4dd3fac
Patricia <patricia.salcedo15@outlook.com>
2020-08-01 15:27:08 +1200

    Index Added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
