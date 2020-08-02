50858d214fd93afadfdceb638b2581f23fa08c0c
NidhiPatel <nidhi.patel@alphacertlabs.com>
2020-08-02 13:06:54 +1200

    connect-buddy

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 app.get("/find", (req, res) => res.sendFile(__dirname + "/public/find.html"));
+app.get("/connectbuddy", (req, res) => res.sendFile(__dirname + "/public/connectbuddy.html"));
 app.get("/endScreen", (req, res) => res.sendFile(__dirname + "/public/endScreen.html"));
 
 app.listen(port, () =>
